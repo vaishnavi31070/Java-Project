eec8ddaa23ce95bae73985ba11357e61514ca3bc
Vaishnavi Jadhav <vaishnavijadhav3107@gmail.com>
2026-09-10 13:10:23 +0530

    Change input method to fixed values for addition
    Refactored AddNumbers to use fixed values instead of user input.

--- AddNumbers.java
+++ AddNumbers.java
@@ -1,22 +1,12 @@
-import java.util.Scanner;
-
 class AddNumbers {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+        int a = 10;
+        int b = 20;
+        int sum = a + b;
 
-        int a, b, sum;
-
-        System.out.print("Enter first number: ");
-        a = sc.nextInt();
-
-        System.out.print("Enter second number: ");
-        b = sc.nextInt();
-
-        sum = a + b;
-
+        System.out.println("Starting Java Program...");
+        System.out.println("First number = " + a);
+        System.out.println("Second number = " + b);
         System.out.println("Sum = " + sum);
-
-        sc.close();
     }
 }
-
